4baa48dfe66e48ae0b0b45befa85c1d31c4e8998
kangwijen <110406976+kangwijen@users.noreply.github.com>
2024-11-29 17:43:43 +0700

    Add FundMe contract and PriceLibrary, remove Counter contract and related files

---
+++ FundMe-foundry/src/PriceLibrary.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+library PriceLibrary {
+    function getPrice() internal view returns(uint256){
+        // 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        // ABI
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (, int256 _price, , ,) = priceFeed.latestRoundData();
+        return uint256(_price * 1e10);
+    }
+
+    function getConversion(uint256 _ethAmount) internal view returns(uint256) {
+        uint256 _ethPrice = getPrice();
+        uint256 _amountInUSD = (_ethPrice * _ethAmount) / 1e18;
+        return _amountInUSD;
+    }
+}
