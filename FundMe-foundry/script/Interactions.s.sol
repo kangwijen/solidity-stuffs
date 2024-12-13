376bfb89acd442d417a85b6751a0f17dc74045be
kangwijen <110406976+kangwijen@users.noreply.github.com>
2024-12-14 01:08:10 +0700

    Add .gitignore and create Interactions script for fund management

---
+++ FundMe-foundry/script/Interactions.s.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import { Script } from "forge-std/Script.sol";
+
+contract SendFundMe is Script {
+    function run () external {
+        // Send funds to the contract
+    }
+}
+
+contract WithdrawFundMe is Script {
+    function run () external {
+        // Withdraw funds from the contract
+    }
+}
